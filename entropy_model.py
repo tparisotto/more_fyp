ab71d56126b6e3263a222c106f349af844be024f
tparisotto <t.parisotto@student.rug.nl>
2020-12-23 11:08:32 +0100

    full dataset indexing fix

--- entropy_model.py
+++ entropy_model.py
@@ -3,7 +3,6 @@ import sys
 import argparse
 from datetime import datetime
 import tensorflow as tf
-from sklearn.model_selection import RepeatedKFold
 from tensorflow import keras
 from tensorflow.keras import layers
 import numpy as np
@@ -22,7 +21,6 @@ otherwise. Hence, each 3D shape is represented by a binary three-dimensional ten
 × 30 voxels. The shape size is normalized such that a cube of 24 × 24 × 24 voxels fully contains the shape, 
 and the remaining empty voxels serve for padding in all directions around the shape. '''
 
-
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # Silence warnings
 timestamp = datetime.now().strftime('%d_%m_%H%M')
 parser = argparse.ArgumentParser()
@@ -31,7 +29,9 @@ parser.add_argument("csvpath")
 args = parser.parse_args()
 BASE_DIR = sys.path[0]
 VOXEL_DATAPATH = args.datapath  # "./voxel_data"
+
 NUM_VIEWS = 60
+SPLIT = 0.9
 
 print(f"Tensorflow v{tf.__version__}")
 
@@ -42,8 +42,10 @@ num_objects = int(data.shape[0] / NUM_VIEWS)
 x = []
 y = []
 
+# TODO: this cycle needs to be pre-computed
 for i in range(num_objects):
-    if i%10==0: print(f'[DEBUG] Now processing {i}/{num_objects}')
+    if i % 100 == 0:
+        print(f'[DEBUG] Now processing {i}/{num_objects}')
     data_subset = data.iloc[NUM_VIEWS * i:NUM_VIEWS * (i + 1)]  # each object is represented by $NUM_VIEWS entries
     labels = utility.get_labels_from_object_views(data_subset)
     voxel_data = np.load(os.path.join(VOXEL_DATAPATH,
@@ -53,8 +55,9 @@ for i in range(num_objects):
     y.append(label_vecs)
 
 
-x_train, y_train = np.array(x[:5]), np.array(y[:5])
-x_test, y_test = np.array(x[5:]), np.array(y[5:])
+n_train = int(num_objects*SPLIT)
+x_train, y_train = np.array(x[:n_train]), np.array(y[:n_train])
+x_test, y_test = np.array(x[n_train:]), np.array(y[n_train:])
 
 model = keras.models.Sequential()
 model.add(layers.Reshape((50, 50, 50, 1)))
@@ -69,7 +72,7 @@ model.compile(optimizer='adam', loss='binary_crossentropy')
 # print(x_train.shape)
 # print(y_train.shape)
 
-model.fit(x_train, y_train, batch_size=8, epochs=5)
+model.fit(x_train, y_train, batch_size=4, epochs=5)
 
 utility.make_dir('./models')
 model.save(f'./models/{timestamp}.h5')
