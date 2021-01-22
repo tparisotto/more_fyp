87bcbcbf6c7092c84b851a8c456a8c5c710e9e6c
tparisotto <t.parisotto@student.rug.nl>
2021-01-22 13:06:06 +0100

    testing mvcnn

--- single_view_cnn.py
+++ single_view_cnn.py
@@ -12,7 +12,7 @@ args = parser.parse_args()
 DATA_PATH = args.data
 
 def load_data():
-    image = keras.preprocessing.image.load_img(os.path.join(DATA_PATH, 'bed_0001_theta_0.0_phi_0.0_vc_0.png'))
+    image = keras.preprocessing.image.load_img(os.path.join(DATA_PATH, 'bathtub_0001_theta_120_phi_0_vc_36.png'))
     print(np.shape(image))
     print(image[0])
 
