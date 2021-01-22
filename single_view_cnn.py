a88e161ca98c9c3be083f8a45e594f663b2c7316
tparisotto <t.parisotto@student.rug.nl>
2021-01-22 13:08:30 +0100

    testing mvcnn

--- single_view_cnn.py
+++ single_view_cnn.py
@@ -14,7 +14,9 @@ DATA_PATH = args.data
 def load_data():
     image = keras.preprocessing.image.load_img(os.path.join(DATA_PATH, 'bathtub_0001_theta_120_phi_0_vc_36.png'))
     print(np.shape(image))
-    print(image[0])
+    image = keras.preprocessing.image.img_to_array(image)
+    print(max(image))
+    print(image)
 
 def main():
     load_data()
