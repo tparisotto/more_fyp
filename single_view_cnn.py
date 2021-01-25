be94d4b45e90d8d239f4f8a17201bf6b310d7b3b
tparisotto <t.parisotto@student.rug.nl>
2021-01-25 17:08:48 +0100

    testing mvcnn

--- single_view_cnn.py
+++ single_view_cnn.py
@@ -1,4 +1,5 @@
 import os
+
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import argparse
 import pandas as pd
@@ -13,7 +14,8 @@ from tensorflow.keras import layers
 tf.compat.v1.logging.set_verbosity(tf.compat.v1.logging.ERROR)
 
 parser = argparse.ArgumentParser()
-parser.add_argument("data", help="Directory where the single views are stored.")
+parser.add_argument("train_data", help="Directory where the single views are stored.")
+parser.add_argument("validation_data")
 parser.add_argument("--batch_size", type=int, default=32)
 parser.add_argument("--epochs", type=int, default=3)
 parser.add_argument("-a", "--architecture", default="vgg", choices=['efficientnet', 'vgg', 'mobilenet', 'mobilenetv2'])
@@ -22,12 +24,19 @@ args = parser.parse_args()
 
 TIMESTAMP = utility.get_datastamp()
 MODEL_DIR = os.path.join(args.out, f"{args.architecture}-{TIMESTAMP}")
-DATA_PATH = args.data
-FILES = os.listdir(DATA_PATH)
-for filename in FILES:  # Removes file without .png extension
+TRAIN_DATA_PATH = args.train_data
+TRAIN_FILES = os.listdir(TRAIN_DATA_PATH)
+for filename in TRAIN_FILES:  # Removes file without .png extension
     if not filename.endswith('png'):
-        FILES.remove(filename)
-NUM_OBJECTS = len(FILES)
+        TRAIN_FILES.remove(filename)
+NUM_OBJECTS_TRAIN = len(TRAIN_FILES)
+
+TEST_DATA_PATH = args.test_data
+TEST_FILES = os.listdir(TEST_DATA_PATH)
+for filename in TEST_FILES:
+    if not filename.endswith('png'):
+        TEST_FILES.remove(filename)
+NUM_OBJECTS_TEST = len(TEST_FILES)
 
 os.makedirs(MODEL_DIR)
 
@@ -43,41 +52,68 @@ METRICS = [
 ]
 CALLBACKS = [
     # tf.keras.callbacks.EarlyStopping(patience=2),
-    tf.keras.callbacks.ModelCheckpoint(filepath=os.path.join(MODEL_DIR, 'model_epoch-{epoch:02d}_ca-{class_accuracy:.2f}_'
-                                                                        'va-{view_accuracy:.2f}.h5'),
-                                       monitor='loss',
-                                       mode='min',
-                                       save_best_only=True,
-                                       save_freq='epoch'),
+    tf.keras.callbacks.ModelCheckpoint(
+        filepath=os.path.join(MODEL_DIR, 'model_epoch-{epoch:02d}_ca-{class_accuracy:.2f}_'
+                                         'va-{view_accuracy:.2f}.h5'),
+        monitor='loss',
+        mode='min',
+        save_best_only=True,
+        save_freq='epoch'),
     tf.keras.callbacks.TensorBoard(log_dir='./logs'),
 ]
 EPOCHS = args.epochs
 BATCH_SIZE = args.batch_size
 
 
-def data_loader():
+def data_loader_train():
     labels_dict = utility.get_label_dict()
-    for i in range(NUM_OBJECTS):
-        file_path = os.path.join(DATA_PATH, FILES[i])
+    for i in range(NUM_OBJECTS_TRAIN):
+        file_path = os.path.join(TRAIN_DATA_PATH, TRAIN_FILES[i])
         x = keras.preprocessing.image.load_img(file_path,
                                                color_mode='rgb',
                                                target_size=(240, 320),
                                                interpolation='nearest')
         x = keras.preprocessing.image.img_to_array(x)
-        label_class = FILES[i].split("_")[0]
+        label_class = TRAIN_FILES[i].split("_")[0]
         if label_class == 'night':
             label_class = 'night_stand'  # Quick fix for label parsing
         label_class = utility.int_to_1hot(labels_dict[label_class], 10)
-        label_view = utility.int_to_1hot(int(FILES[i].split("_")[-1].split(".")[0]), 60)
+        label_view = utility.int_to_1hot(int(TRAIN_FILES[i].split("_")[-1].split(".")[0]), 60)
         yield x, (label_class, label_view)
 
 
-def dataset_generator():
-    dataset = tf.data.Dataset.from_generator(data_loader,
+def data_loader_test():
+    labels_dict = utility.get_label_dict()
+    for i in range(NUM_OBJECTS_TEST):
+        file_path = os.path.join(TEST_DATA_PATH, TEST_FILES[i])
+        x = keras.preprocessing.image.load_img(file_path,
+                                               color_mode='rgb',
+                                               target_size=(240, 320),
+                                               interpolation='nearest')
+        x = keras.preprocessing.image.img_to_array(x)
+        label_class = TEST_FILES[i].split("_")[0]
+        if label_class == 'night':
+            label_class = 'night_stand'  # Quick fix for label parsing
+        label_class = utility.int_to_1hot(labels_dict[label_class], 10)
+        label_view = utility.int_to_1hot(int(TEST_FILES[i].split("_")[-1].split(".")[0]), 60)
+        yield x, (label_class, label_view)
+
+
+def dataset_generator_train():
+    dataset = tf.data.Dataset.from_generator(data_loader_train,
                                              output_types=(tf.float32, (tf.int16, tf.int16)),
                                              output_shapes=(tf.TensorShape([240, 320, 3]),
                                                             (tf.TensorShape([10]), tf.TensorShape([60]))))
     dataset = dataset.repeat(EPOCHS)
+    dataset = dataset.batch(BATCH_SIZE)
+    return dataset
+
+
+def dataset_generator_test():
+    dataset = tf.data.Dataset.from_generator(data_loader_test,
+                                             output_types=(tf.float32, (tf.int16, tf.int16)),
+                                             output_shapes=(tf.TensorShape([240, 320, 3]),
+                                                            (tf.TensorShape([10]), tf.TensorShape([60]))))
     dataset = dataset.batch(BATCH_SIZE)
     return dataset
 
@@ -121,9 +157,10 @@ def generate_cnn(app="efficientnet"):
 
 def main():
     model = generate_cnn(app=args.architecture)
-    num_batches = int(NUM_OBJECTS / BATCH_SIZE)
-    data_gen = dataset_generator()
-    history = model.fit(data_gen, steps_per_epoch=num_batches, epochs=EPOCHS, callbacks=CALLBACKS)
+    num_batches = int(NUM_OBJECTS_TRAIN / BATCH_SIZE)
+    train_data_gen = dataset_generator_train()
+    test_data_gen = data_loader_test()
+    history = model.fit(train_data_gen, steps_per_epoch=num_batches, epochs=EPOCHS, callbacks=CALLBACKS, validation_data=test_data_gen)
     hist_df = pd.DataFrame(history.history)
     hist_df.to_csv(os.path.join(f"class-view_training_history.csv"))
 
