2ec305bb53becff87fe3f4c8b6758d5c0b432a15
tparisotto <t.parisotto@student.rug.nl>
2021-01-30 10:49:40 +0100

    testing mvcnn

--- single_view_cnn.py
+++ single_view_cnn.py
@@ -70,7 +70,7 @@ def scheduler(epoch, lr):
 CALLBACKS = [
     # tf.keras.callbacks.EarlyStopping(patience=3),
     tf.keras.callbacks.ModelCheckpoint(
-        filepath=os.path.join(MODEL_DIR, 'model_epoch-{epoch:02d}_loss-{val_loss:.3f}.h5'),
+        filepath=os.path.join(MODEL_DIR, 'model_epoch-{epoch:03d}_loss-{val_loss:.3f}.h5'),
         monitor='val_loss',
         mode='min',
         save_best_only=True,
