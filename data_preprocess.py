23c7d6afd1bd71ec857fc651c7b10b0d93537807
tparisotto <t.parisotto@student.rug.nl>
2021-02-02 17:04:00 +0100

    testing mvcnn

--- data_preprocess.py
+++ data_preprocess.py
@@ -7,7 +7,7 @@ import pandas as pd
 import numpy as np
 
 parser = argparse.ArgumentParser()
-parser.add_argument("datapath")
+# parser.add_argument("datapath")
 parser.add_argument("csvpath")
 args = parser.parse_args()
 csv_filename = args.csvpath
@@ -16,18 +16,18 @@ data = pd.read_csv(csv_filename, engine='python')
 NUM_VIEWS = 60
 num_objects = int(data.shape[0] / NUM_VIEWS)
 
-x = []
+# x = []
 y = []
 print(f"[INFO] Processing {num_objects} objects into numpy arrays...")
 for i in trange(num_objects):
     data_subset = data.iloc[NUM_VIEWS * i:NUM_VIEWS * (i + 1)]  # each object is represented by $NUM_VIEWS entries
     labels = utility.get_labels_from_object_views(data_subset)
-    voxel_data = np.load(os.path.join(VOXEL_DATAPATH,
-                                      f"{list(data_subset['label'])[0]}_{list(data_subset['obj_ind'])[0]:04d}.npy"))
+    # voxel_data = np.load(os.path.join(VOXEL_DATAPATH,
+                                      # f"{list(data_subset['label'])[0]}_{list(data_subset['obj_ind'])[0]:04d}.npy"))
     label_vecs = utility.view_vector(labels, NUM_VIEWS)
-    x.append(voxel_data)
+    # x.append(voxel_data)
     y.append(label_vecs)
 
-np.save('x_data.npy', x)
-np.save('y_data.npy', y)
+# np.save('x_data.npy', x)
+np.save('y_data_v2.npy', y)
 print("[INFO] Features (x_data.npy) and Labels (y_data.npy) successfully stored in current directory.")
--- utility.py
+++ utility.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy.signal as sig
+from skimage.feature import peak_local_max
 import os
 import shutil
 import datetime
@@ -26,69 +26,36 @@ def make_dir(path, delete=False):
 
 
 def extract_labels(data):
-    data = data.rename(columns={'rot_x': 'theta', 'rot_y': 'phi'})
-    # fig, ax = plt.subplots(5, 1, sharey=True)
-    maxima = []
-    maxima_entropy = []
-    for i in range(0, 5):
-        sub = data[data['theta'] == 30.0 * (i + 1)]
-        x = np.array(sub['phi'])
-        y = np.array(sub['entropy'])
-        sub = zip(x, y)
-        subsort = sorted(sub)
-        tuples = zip(*subsort)
-        x, y = [np.array(el) for el in tuples]
-        # Trick to have the first or last element of the signal as a maximum
-        ext, _ = sig.find_peaks(np.concatenate(([y[-1]], y, [y[0]])))
-        ext = ext - 1
-        maxima.append(ext)
-        maxima_entropy.append(y)
-        # ax[i].plot(x, y)
-        # ax[i].grid()
-        # ax[i].scatter(x[ext], y[ext], c='red')
-        # ax[i].set_title(f"theta = {30.0*(i+1)}")
-    maxima = np.array(maxima)
-    maxima_entropy = np.array(maxima_entropy)
-    views = np.zeros(12)
-    views_entropy = np.zeros(12)
-    for j in range(0, 5):
-        for i in range(0, 12):
-            if i in maxima[j]:
-                if maxima_entropy[j, i] > views_entropy[i]:
-                    views_entropy[i] = maxima_entropy[j, i]
-                    views[i] = j + 1
-    # print(views)
-
+    mat = np.zeros((60, 1))
+    for i in range(0, 60):
+        entropy = float(data[data['view_code'] == i].entropy)
+        mat[i] = entropy
+    mat.resize((5, 12))
+    coords = peak_local_max(mat, min_distance=1, exclude_border=False)
     labels = []
-    for i in range(0, 12):
-        if views[i] > 0:
-            labels.append((views[i] * 30.0, i * 30.0))
-    return labels
+    for (y, x) in coords:
+        labels.append((y * 12) + x)
+    # fig, ax = plt.subplots(1)
+    # ax.imshow(mat, cmap='rainbow')
+    # for i in range(len(coords)):
+    #     circle = plt.Circle((coords[i][1], coords[i][0]), radius=0.2, color='black')
+    #     ax.add_patch(circle)
+    #
+    # plt.xticks([i for i in range(12)], [i*30 for i in range(12)])
+    # plt.yticks([i for i in range(5)], [(i+1) * 30 for i in range(5)])
     # plt.show()
+
+    return labels
+
 
 
 def get_labels_from_object_views(data):
-    ### Dictionaries ###
-    label2idx = {}
-    count = 0
-    for theta in range(30, 151, 30):
-        for phi in range(0, 331, 30):
-            label2idx[theta, phi] = count
-            count += 1
-
-    idx2label = {}
-    count = 0
-    for theta in range(30, 151, 30):
-        for phi in range(0, 331, 30):
-            idx2label[count] = (theta, phi)
-            count += 1
-    #######################
     subset_labels = extract_labels(data)
-    subset_idx = []
-    for lab in subset_labels:
-        subset_idx.append(label2idx[lab])
-    subset_idx.sort()
-    return subset_idx
+    # subset_idx = []
+    # for lab in subset_labels:
+    #     subset_idx.append(label2idx[lab])
+    subset_labels.sort()
+    return subset_labels
 
 
 def get_label_dict(inverse=False):
